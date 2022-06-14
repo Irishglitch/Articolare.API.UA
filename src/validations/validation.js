9f59f137ce742eb572b5630b7ce0e5145c87be75
Irishglitch <murilozs@live.ie>
2022-06-14 19:49:39 +0100

    Validations added

---
+++ src/validations/validation.js
@@ -0,0 +1,39 @@
+/**
+ * The validation.js provides essential information about all required user authentication.
+ * This file should not be amended without confirmation first. 
+ * @version 1.0.1
+ * @author Murilo Silvestre | Property of Articolare Ltd
+ * @email info@foireann.com
+ */
+
+// Libraries
+const joi = require('joi')
+
+
+// Functions
+// User registration validation 
+const userRegistrationValidation = (data) =>{
+    const userSchemaValidation = joi.object({
+        name:joi.string().required().min(3).max(256),
+        lastName:joi.string().required().min(3).max(256),
+        email:joi.string().required().min(3).max(256).email(),
+        password:joi.string().required().min(8).max(1026)
+    })
+    // returning the user validation 
+    return userSchemaValidation.validate(data)
+}
+
+// Login vaidations
+const userLoginValidation = (data) =>{
+    const userLoginSchemaValidation = joi.object({
+        email:joi.string().required().min(3).max(256).email(),
+        password:joi.string().required().min(8).max(1026)
+    })
+    // returning the login user validation
+    return userLoginSchemaValidation.validate(data)
+}
+
+
+// Module Exports
+module.exports.userRegistrationValidation = userRegistrationValidation
+module.exports.userLoginValidation = userLoginValidation
