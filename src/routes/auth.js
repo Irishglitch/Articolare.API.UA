cebd073b6ea9ddc8ba969c81d0de34247dfa922a
Nycolas Silvestre <nycolaszsilvestre@gmail.com>
2022-10-23 19:28:58 +0100

    add passwordRecovery/token endpoint

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -65,13 +65,13 @@ router.post('/login', async(req, res) => {
     // Validation 2 - Check user is not registered
     const userExists = await User.findOne({email:req.body.email})
     if(!userExists){
-        return res.status(400).send({message:'Email account not found. Please log in with a valid account'})
+        return res.status(400).send({message:'Invalid credentials information. Please check your credentials an try again.'})
     }
 
     // Validation 3 - Password decription and validation
     const userPasswordValidation = await bcryptjs.compare(req.body.password, userExists.password)
     if(!userPasswordValidation){
-        return res.status(400).send({message:'Password is wrong'})
+        return res.status(400).send({message:'Invalid credentials information. Please check your credentials an try again.'})
     }
 
     // Auth-Token Generation
@@ -94,28 +94,65 @@ router.patch('/recoveryPassword',async(req, res) => {
         curUser.updatedAt = now;
         curUser.save();
     }
-
+    sendMail();
+    //TODO ADD REAL EMAIL TEMPLATE
     res.end();
+})
+
+router.patch('/recoveryPassword/:token',async(req, res) => {
+    // const {error} = passwordRecoveryValidation(req.body)
+    //TODO CREATE REQUIRED VALIDATION
+
+    // if(error) {
+    //     // Validation 1 - Summarised error message
+    //     return res.status(400).send({message: error['details'][0]['message']})
+    // }
+    const token = req.params['token'];
+    const curUser = await User.findOne({passwordRecoveryToken:token})
+    if(curUser){
+        const now = Date.now();
+
+        if(curUser.recoveryTokenExpireDate <= now){
+            res.status(400).send('Error: Expired password recovery token.')
+            return;
+        }
+
+        const salt = await bcryptjs.genSalt(8)
+        const hashedPassword = await bcryptjs.hash(req.body.password,salt)
+
+        curUser.recoveryTokenExpireDate = (now - 1000);
+        curUser.updatedAt = now;
+        curUser.password = hashedPassword;
+        curUser.save();
+        res.end();
+        return;
+    }
+    else{
+        res.status(400).send('Error: Invalid password recovery token.')
+        return;
+    }
 
 })
 
 
-function sendMail(){
+function sendMail(mailAddres){ //TODO CREATE EMAIL SERVICE WITH DYNAMIC TEMPLATES
+    const user = process.env.MAIL_ADDRESS;
+    const pass = process.env.PASS;
     var nodemailer = require('nodemailer');
 
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: 'outlook',
         auth: {
-            user: 'youremail@gmail.com',
-            pass: 'yourpassword'
+            user: user,
+            pass: pass
         }
     });
 
     var mailOptions = {
-        from: 'youremail@gmail.com',
-        to: 'myfriend@yahoo.com',
+        from: user,
+        to: mailAddres,
         subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
+        html: '<!DOCTYPE htmlPUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head>    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />    <meta http-equiv="X-UA-Compatible" content="IE=edge" />    <meta name="viewport" content="width=device-width, initial-scale=1.0">    <title>Articolare</title>    <style type="text/css">        body {            Margin: 0;            padding: 0;            background-color: white;        }        table {            border-spacing: 0;        }        td {            padding: 0;        }        img {            border: 0;        }        .wrapper {            width: 100%;            table-layout: fixed;            background-color: white;            padding-bottom: 40px;        }        .webkit {            max-width: 600px;            background-color: white;        }        .outer {            Margin: 0 auto;            width: 100%;            max-width: 600px;            border-spacing: 0;            font-family: sans-serif;            color: #4a4a4a;        }        @media screen and (max-width: 600px) {}        @media screen and (max-width: 400px) {}    </style></head><body>    <center class="wrapper">        <div class="webkit">            <table class="outer" align="center">                <tr>                    <td>                        <table style="width: 100%; border-spacing: 0; padding-top: 3rem;">                            <tr>                                <td style="text-align: center;"><a href="https://articolare.com"><img                                            src="https://storage.googleapis.com/assets-articolare/mainLogoLight.png"                                            width="180" alt="Logo" title="Logo"></a></td>                            </tr>                        </table>                    </td>                </tr>                <tr>                    <td><br><br>                        <p>Hello [#user_name#],</p>                        <p style="max-width: 100; font-weight: 400;">This is a password recovery email. </p><br>                        <p>[#email_confirmation_link#]</p>                    </td>                </tr>                <tr>                    <td><br>                        <table style="width: 100%; border-spacing: 0; padding-top: 3rem;">                            <tr>                                <td style="text-align: left;"><a href="https://articolare.com"><img                                            src="https://storage.googleapis.com/assets-articolare/better-writing.png"                                            width="600" alt="Logo" title="Logo"></a></td>                            </tr>                        </table>                        <p style="font-size: 13px;">By clicking on the link above you are agreeing to our terms                            andconditions. </p>                        <p style="font-size: 13px;">All rights reserved. Copyright © 2021 Foireann | Made with <spam                                style="color: #ff0000; font-size: large;">♥</spam> by ourteam in London, Lisbon,                            &Milano.</p>                    </td>                </tr>            </table>        </div>    </center></body></html>'
     };
 
     transporter.sendMail(mailOptions, function(error, info){
